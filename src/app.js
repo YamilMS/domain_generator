aefb5574b813b82f5d43ab8e218e49fa3675d1ea
YamilMS <yoms.tf@gmail.com>
2022-08-13 11:40:51 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let start = ["The", "Your", "Our"];
+  let mid = ["Cat", "Mom", "Car"];
+  let end = ["Blue", "Satanic", "Cute"];
+  let TLD = [".com", ".org", ".edu"];
+
+  // SOLUTION USING NESTED LOOPS
+  for (let i in start) {
+    for (let x in mid) {
+      for (let j in end) {
+        for (let y in TLD) {
+          console.log(`${start[i]}${mid[x]}${end[j]}${TLD[y]}`);
+        }
+      }
+    }
+  }
+  // ---------------------------------------------------
+
+  //  SOLUTION USING MAP INSPIRED BY @zjovic
+  const domainName = start.flatMap(a => {
+    return mid.flatMap(b => end.flatMap(c => TLD.map(d => `${a}${b}${c}${d}`)));
+  });
+  console.log(domainName.join(" "));
+  //----------------------------------------------------
 };
